8bb0b08b4758e5b2373e0c34f20a16be1e4d2bf8
windend <fengfengwin@yeah.net>
2018-10-07 02:23:27 +0800

    5th commmt

---
+++ miniprogram/zujian/model1s/model1.js
@@ -0,0 +1,43 @@
+// zujian/model3/model3.js
+var common = require('../common.js')
+
+behaviors: [common],
+
+
+  Component({
+    /**
+     * 组件的属性列表
+     */
+    behaviors: [common],
+    properties: {
+      detail: {
+        type: Object
+      },
+      h: {
+        type: Number
+      }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+      addcart: function(e) {
+        this.triggerEvent('addcart', {
+          itemid: e.currentTarget.dataset.id
+        })
+      },
+      itemdetail: function(e) {
+        this.triggerEvent('itemdetail', {
+          itemid: e.currentTarget.dataset.id
+        })
+      },
+    }
+  })
